00fb02b594118e6094c0f590c76415eca487af26
Pranav Sinha <pran.sinha1.0@gmail.com>
2023-02-09 12:56:04 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const fetchSongs = async () => {
+    fetch("https://itunes.apple.com/search?term=radiohead")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+  fetchSongs();
   return (
     <div className="App">
       <header className="App-header">
@@ -14,8 +21,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
